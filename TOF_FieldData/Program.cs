f38e00992ae90f0925764578fdfacb923ef6ad3c
Unknown <dan.kaste@gmail.com>
2018-05-18 21:37:48 -0400

    Added unzipping

--- TOF_FieldData/Program.cs
+++ TOF_FieldData/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.IO;
+using System.IO.Compression;
 
 namespace TOF_FieldData
 {
     class Program
     {
+        private static string dataDir = "/Users/dan.kaste/Code/TOF_FieldData/TestData";
+        private static string exportDir = "/Users/dan.kaste/Code/TOF_FieldData/TestData/Unzipped";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach (var file in Directory.GetFiles(dataDir))
+            {
+                if (file.Contains(".zip"))
+                {
+                    Console.WriteLine($"Extracting file {file}");
+
+                    ZipFile.ExtractToDirectory(file, exportDir);
+                }
+            }
         }
     }
 }
+
